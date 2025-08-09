a9398676cd6e849f884d7204a6e3eb2ffef89eaa
cyypherus <ejjonny@gmail.com>
2025-08-09 14:37:28 -0600

    fix link

--- Casks/idle-hue.rb
+++ Casks/idle-hue.rb
@@ -3,12 +3,12 @@ cask "idle-hue" do
 
   on_arm do
     sha256 "c8cc35ef9598ec74dcb877e33e90f88281963f5642c21cc74c00f3893ce305bb"
-    url "https://github.com/cyypherus/cyy-apps/releases/download/#{version}/idle-hue-#{version}-macos-arm.zip"
+    url "https://github.com/cyypherus/cyy-apps/releases/download/#{version}/idle-hue-macos-arm.zip"
   end
 
   on_intel do
     sha256 "0195b159f309cd49dd8da7f9983f423c11fdc0c2e25eda18d4a43277fa939f0f"
-    url "https://github.com/cyypherus/cyy-apps/releases/download/#{version}/idle-hue-#{version}-macos-intel.zip"
+    url "https://github.com/cyypherus/cyy-apps/releases/download/#{version}/idle-hue-macos-intel.zip"
   end
   name "idle-hue"
   homepage "https://github.com/cyypherus"
