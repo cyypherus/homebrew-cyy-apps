a09db5860619d63d07baf2ad915632f32c4e7879
cyypherus <ejjonny@gmail.com>
2025-08-09 01:32:28 -0600

    fix hash

--- Casks/idle-hue.rb
+++ Casks/idle-hue.rb
@@ -1,6 +1,6 @@
 cask "idle-hue" do
   version "0.1.0"
-  sha256 "sha256:f7827040610033d5aa7e8f19002ca0bc2f32c75c51e38edb8c3ca780e93b3f7f"
+  sha256 "f7827040610033d5aa7e8f19002ca0bc2f32c75c51e38edb8c3ca780e93b3f7f"
 
   url "https://github.com/cyypherus/cyy-apps/releases/download/#{version}/idle-hue-#{version}-macos-arm64.zip"
   name "idle-hue"
