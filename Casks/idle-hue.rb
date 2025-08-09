ad0b86d69837959c12ada4838ec2c0b3978756d1
cyypherus <ejjonny@gmail.com>
2025-08-09 14:36:38 -0600

    add intel, updates

--- Casks/idle-hue.rb
+++ Casks/idle-hue.rb
@@ -1,8 +1,15 @@
 cask "idle-hue" do
-  version "0.1.0"
-  sha256 "f7827040610033d5aa7e8f19002ca0bc2f32c75c51e38edb8c3ca780e93b3f7f"
+  version "0.2.0"
 
-  url "https://github.com/cyypherus/cyy-apps/releases/download/#{version}/idle-hue-#{version}-macos-arm64.zip"
+  on_arm do
+    sha256 "c8cc35ef9598ec74dcb877e33e90f88281963f5642c21cc74c00f3893ce305bb"
+    url "https://github.com/cyypherus/cyy-apps/releases/download/#{version}/idle-hue-#{version}-macos-arm.zip"
+  end
+
+  on_intel do
+    sha256 "0195b159f309cd49dd8da7f9983f423c11fdc0c2e25eda18d4a43277fa939f0f"
+    url "https://github.com/cyypherus/cyy-apps/releases/download/#{version}/idle-hue-#{version}-macos-intel.zip"
+  end
   name "idle-hue"
   homepage "https://github.com/cyypherus"
 
