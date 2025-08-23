c131bb049f5a17aafb3343a9efceee1e1b08f9a8
cyypherus <ejjonny@gmail.com>
2025-08-23 02:04:32 -0600

    Update idle-hue.rb

--- Casks/idle-hue.rb
+++ Casks/idle-hue.rb
@@ -1,13 +1,13 @@
 cask "idle-hue" do
-  version "0.4.2"
+  version "0.4.4"
 
   on_arm do
-    sha256 "e02e07eee385987d7db0f8b6921bc782d064ea08c7245aadb737db2c75036310"
+    sha256 "265f4e19c75dbc3b7b01feeaa3b73a6010e291b0048de8f4380eedfb25fa0851"
     url "https://apps.cyypher.com/idle-hue/download/macos-arm/#{version}"
   end
 
   on_intel do
-    sha256 "102f5c4884208bae79b5b5cf4d9f77326c8c80c3d8b2d51617616b9caca05616"
+    sha256 "8e50ded5331c14cc11ecbd9e69c104471ff9d42027a9b7ed0bef11913e1a5e75"
     url "https://apps.cyypher.com/idle-hue/download/macos-intel/#{version}"
   end
   name "idle-hue"
